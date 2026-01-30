05d5f29ce292e74e7569058a4b1ee7a56b22ab6d
chaves99 <viniciusbaleia1999@gmail.com>
2026-01-30 17:26:20 -0300

    feat: debuging build

--- src/main/java/com/menuonline/MenuOnlineApplication.java
+++ src/main/java/com/menuonline/MenuOnlineApplication.java
@@ -1,17 +1,34 @@
 package com.menuonline;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import lombok.extern.slf4j.Slf4j;
 
 @EnableJpaAuditing
 @EnableJpaRepositories
 @SpringBootApplication
+@Slf4j
 public class MenuOnlineApplication {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 
     public static void main(String[] args) {
         SpringApplication.run(MenuOnlineApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner() {
+        return (args) -> {
+            System.out.println("#####################");
+            log.info("main - dbUrl: {}", dbUrl);
+        };
     }
 
 }
